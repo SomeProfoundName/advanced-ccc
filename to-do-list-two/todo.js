4233f88da19cb66417f7dd7fb9ff34eaf5596762
Collins <84730393+SomeProfoundName@users.noreply.github.com>
2024-05-26 13:59:08 +0100

    first commit week two assignments

---
+++ to-do-list-two/todo.js
@@ -0,0 +1,43 @@
+const todoList = ['Make Tea', 'Watch YouTube']
+
+renderToDoList()
+
+function renderToDoList(){
+    let todoListHTML = ''
+
+    todoList.forEach((todoObject, index) => {
+        const name = todoObject;
+        const html = `
+        <div class="item">
+            <p>${name}</p>
+            <button type="button" class="btn btn-outline-danger js-delete-button">Delete</button>
+        </div>
+        `;
+        todoListHTML += html
+    });
+    document.querySelector('.todo-input').innerHTML = todoListHTML;
+
+    document.querySelectorAll('.js-delete-button').forEach((deleteButton, index) => {
+        deleteButton.addEventListener('click', () => {
+            todoList.splice(index, 1);
+            renderToDoList();
+        });
+    });
+    
+}
+
+function addTodo() {
+    const todoInputElement = document.querySelector('.js-todo-input');
+    const todo = todoInputElement.value;
+
+    if (todoInputElement.value = '') {
+        return;
+    } else {
+        todoList.push(todo);
+        console.log(todoList);
+
+    todoInputElement.value = '';
+
+    renderToDoList();
+    }
+};
