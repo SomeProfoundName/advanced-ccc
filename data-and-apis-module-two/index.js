ae9a08a8b33a54833f086151f77859c36e38dd2f
Collins <84730393+SomeProfoundName@users.noreply.github.com>
2024-05-31 16:30:10 +0100

    node js, post and get

--- data-and-apis-module-two/index.js
+++ data-and-apis-module-two/index.js
@@ -2,3 +2,14 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening @ 3000'));
 app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}))
+
+app.post('/api', (request, responce) => {
+    console.log(request.body);
+    const data = request.body;
+    responce.json({
+        status: 'success',
+        latitude: data.latitude,
+        longitude: data.longitude
+    });
+})
