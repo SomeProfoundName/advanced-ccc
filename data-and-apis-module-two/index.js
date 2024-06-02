e04557674e9539e22d51c989f35b7955c33bdc5d
Collins <84730393+SomeProfoundName@users.noreply.github.com>
2024-06-02 18:54:56 +0100

    fetch get and databases with webpage styling

--- data-and-apis-module-two/index.js
+++ data-and-apis-module-two/index.js
@@ -1,15 +1,35 @@
 const express = require('express');
+const Datastore = require('nedb')
 const app = express();
 app.listen(3000, () => console.log('listening @ 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}))
 
-app.post('/api', (request, responce) => {
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if (err) {
+            responce.end();
+            return;
+        }
+    response.json(data);
+    });
+});
+
+app.post('/api', (request, response) => {
     console.log(request.body);
     const data = request.body;
-    responce.json({
-        status: 'success',
-        latitude: data.latitude,
-        longitude: data.longitude
-    });
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data)
+    response.json(data)
+    // responce.json({
+    //     status: 'success',
+    //     name: data.name,
+    //     latitude: data.latitude,
+    //     longitude: data.longitude,
+    //     timestamp: timestamp,
+    // });
 })
